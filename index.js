66acad992b43a06f63b45c64bedd1ebf2785ac21
Gerardo <gerardozuniga133@gmail.com>
2026-02-27 19:21:01 -0600

    API fetch completed

--- index.js
+++ index.js
@@ -49,3 +49,26 @@ messageForm.addEventListener("submit", function (event) {
 
   messageForm.reset();
 });
+
+//=== Fetching GitHub repos ===//////
+
+fetch("https://api.github.com/users/germxz/repos")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (repositories) {
+    console.log("repositories", repositories);
+
+    //code to get the projects section and create list items for each repository
+    const projectSection = document.getElementById("Projects");
+    const projectList = projectSection.querySelector("ul");
+
+    for (let i = 0; i < repositories.length; i++) {
+      const project = document.createElement("li");
+      project.innerText = repositories[i].name;
+      projectList.appendChild(project);
+    }
+  })
+  .catch(function (error) {
+    console.log("error", error);
+  });
